7516238fa4d0012a8c5da0509468ee2b50a7382c
VladoDotNet, Ing. de sistemas y Diseño Web, .Net developer <vladodotnet@gmail.com>
2022-11-29 23:04:54 -0500

    .
    .

--- PATRONESAPP/UIState/ProgramState.cs
+++ PATRONESAPP/UIState/ProgramState.cs
@@ -1,9 +1,4 @@
 ﻿using CORE.Servicios.State;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PATRONESAPP.UIState
 {
@@ -22,11 +17,6 @@ namespace PATRONESAPP.UIState
                 Console.WriteLine("PROCESO DE MATRÍCULA ");
                 Console.WriteLine("------------------------");
 
-                //Console.Write("Solicitar xxxxx\n" +
-                //    "[1] xxxx,\n" +
-                //    "[2] xxxx,\n" +
-                //    "[3] xxxx : ");
-                //var opcion = Convert.ToInt32(Console.ReadLine());
 
                 EstadoContext estadoContext = new EstadoContext(new EstadoSolicitud());
                 estadoContext.Request();
@@ -49,9 +39,9 @@ namespace PATRONESAPP.UIState
                 continuar = Console.ReadKey().KeyChar;
                 Console.Clear();
 
-                Console.WriteLine("---------------------------------------");
-                Console.WriteLine(" PATRONES - xxxxx ");
-                Console.WriteLine("---------------------------------------");
+                Console.WriteLine("-------------------------");
+                Console.WriteLine(" PATRONES - State ");
+                Console.WriteLine("-------------------------");
 
             } while (continuar != 'n');
         }
