40bcae4aefb496b23b48cb8ce062ba0ef0e7a5e7
VladoDotNet, Ing. de sistemas y Diseño Web, .Net developer <vladodotnet@gmail.com>
2022-12-02 09:22:15 -0500

    .
    .

--- PATRONESAPP/UIAdapter/ProgramAdapter.cs
+++ PATRONESAPP/UIAdapter/ProgramAdapter.cs
@@ -1,42 +1,42 @@
-﻿using CORE.Interfaces.Adapter;
-using CORE.Servicios.Adapter;
+﻿//using CORE.Interfaces.Adapter;
+//using CORE.Servicios.Adapter;
 
-namespace PATRONESAPP.UIAdapter
-{
-    public class ProgramAdapter
-    {
-        static async Task Main(string[] arg)
-        {
-            ProfesorPlanta ProfesorPlanta = new ProfesorPlanta();
-            AdapterPVirtual adapterPVirtual = new AdapterPVirtual();
+//namespace PATRONESAPP.UIAdapter
+//{
+//    public class ProgramAdapter
+//    {
+//        static async Task Main(string[] arg)
+//        {
+//            ProfesorPlanta ProfesorPlanta = new ProfesorPlanta();
+//            AdapterPVirtual adapterPVirtual = new AdapterPVirtual();
 
-            Console.WriteLine("-----------------------------");
-            Console.WriteLine(" PATRONES - Adapter  ");
-            Console.WriteLine("-----------------------------");
+//            Console.WriteLine("-----------------------------");
+//            Console.WriteLine(" PATRONES - Adapter  ");
+//            Console.WriteLine("-----------------------------");
 
-            char continuar = default;
-            Console.WriteLine("\n");
-            Console.WriteLine("GESTION CARGAS DE PROFESORES");
-            Console.WriteLine("------------------------------");
+//            char continuar = default;
+//            Console.WriteLine("\n");
+//            Console.WriteLine("GESTION CARGAS DE PROFESORES");
+//            Console.WriteLine("------------------------------");
 
-            Console.Write("ANTES de insertar el ADAPTEE . . .\n");
+//            Console.Write("ANTES de insertar el ADAPTEE . . .\n");
 
-            Console.WriteLine("\n");
+//            Console.WriteLine("\n");
 
-            ProfesorPlanta.Cursos();
-            ProfesorPlanta.Salones();
-            ProfesorPlanta.AgendaHorarios();
-            ProfesorPlanta.AtencionEstudiantes();
-            await Task.Delay(5000);
+//            ProfesorPlanta.Cursos();
+//            ProfesorPlanta.Salones();
+//            ProfesorPlanta.AgendaHorarios();
+//            ProfesorPlanta.AtencionEstudiantes();
+//            await Task.Delay(5000);
 
-            Console.WriteLine("\n");
+//            Console.WriteLine("\n");
 
-            Console.Write("INSERTANDO el ADAPTEE . . .\n");
-            await Task.Delay(3000);
-            adapterPVirtual.Cursos();
-            adapterPVirtual.Salones();
-            adapterPVirtual.AgendaHorarios();
-            adapterPVirtual.AtencionEstudiantes();
-        }
-    }
-}
+//            Console.Write("INSERTANDO el ADAPTEE . . .\n");
+//            await Task.Delay(3000);
+//            adapterPVirtual.Cursos();
+//            adapterPVirtual.Salones();
+//            adapterPVirtual.AgendaHorarios();
+//            adapterPVirtual.AtencionEstudiantes();
+//        }
+//    }
+//}
