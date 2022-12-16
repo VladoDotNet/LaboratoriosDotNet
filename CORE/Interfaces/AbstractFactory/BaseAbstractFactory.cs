36b1b8b1520fd309f38b955f0085b3f73134e9cd
VladoDotNet, Ing. de sistemas y Diseño Web, .Net developer <vladodotnet@gmail.com>
2022-12-16 07:17:09 -0500

    .
    .

---
+++ CORE/Interfaces/AbstractFactory/BaseAbstractFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Interfaces.AbstractFactory
+{
+    public abstract class BaseAbstractFactory
+    {
+        public abstract IPrySeminarios CrearProyectoSeminarios(string modalidad);
+        public abstract IPryInvestigacion CrearProyectosInvestigacion(string modalidad);
+    }
+}
---
+++ CORE/Servicios/Adapter/Adapter.cs
@@ -0,0 +1,31 @@
+﻿using CORE.Interfaces.Adapter;
+
+namespace CORE.Servicios.Adapter
+{
+    public class Adapter : ITargetBase
+    {
+        private readonly AdapteeServicio _adapatee;
+
+        public Adapter(AdapteeServicio adapateeServicio) //<-- Constructor
+        {
+            _adapatee = adapateeServicio;
+        }
+        
+        public async Task PrecesarFormatosEscuela(string formato)
+        {
+            switch (formato)
+            {
+                case string s when s == "XML" || s == "JSON" || s == "TEXTO PLANO":
+                    Console.WriteLine("PROCESANDO . . .  Formatos de la Escuela\n ");
+                    await Task.Delay(3000);
+                    Console.WriteLine($"Formateo a {formato} exitoso!\n ");
+                    break;
+                default:
+                    Console.WriteLine("PROCESANDO . . .  Formatos especiales\n ");
+                    var ConversionOtro =  await _adapatee.ProcesarRequerimientos(formato);
+                    Console.WriteLine(ConversionOtro);
+                    break;
+            }
+        }
+    }
+}
